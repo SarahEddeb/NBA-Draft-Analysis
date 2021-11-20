a8d228893b38f22cead48dbcc9a824ac1f7f8569
Sarah Eddeb <saraheddeb@Sarahs-MacBook-Pro.local>
2021-11-19 20:34:14 -0500

    Fix file system to match required format

--- nbaData.sql
+++ nbaData.sql
@@ -1,7 +1,7 @@
-\copy PlayerInfo  FROM 'data/playerInfoData2020.csv' DELIMITER ',' CSV
-\copy DraftInfo  FROM 'data/draftResults2020.csv' DELIMITER ',' CSV
-\copy Career  FROM 'data/careerData2020.csv' DELIMITER ',' CSV
-\copy BodyMeasurements  FROM 'data/combineAnthro2020.csv' DELIMITER ',' CSV
-\copy StrengthAgility  FROM 'data/combineStrength2020.csv' DELIMITER ',' CSV
-\copy Shooting  FROM 'data/shootingStats2020.csv' DELIMITER ',' CSV
-\copy AverageGameStats  FROM 'data/gameStats2020.csv' DELIMITER ',' CSV
+\copy PlayerInfo  FROM 'playerInfoData2020.csv' DELIMITER ',' CSV
+\copy DraftInfo  FROM 'draftResults2020.csv' DELIMITER ',' CSV
+\copy Career  FROM 'careerData2020.csv' DELIMITER ',' CSV
+\copy BodyMeasurements  FROM 'combineAnthro2020.csv' DELIMITER ',' CSV
+\copy StrengthAgility  FROM 'combineStrength2020.csv' DELIMITER ',' CSV
+\copy Shooting  FROM 'shootingStats2020.csv' DELIMITER ',' CSV
+\copy AverageGameStats  FROM 'gameStats2020.csv' DELIMITER ',' CSV
