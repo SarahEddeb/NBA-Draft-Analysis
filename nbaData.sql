f2c4efc95405d366fd0bc0276f681123216897e3
Sarah Eddeb <saraheddeb@utmuser171-181.wireless.utoronto.ca>
2021-12-02 15:12:48 -0500

    Add data sql files for 2018 and 2019

--- nbaData.sql
+++
@@ -1,7 +0,0 @@
-\copy PlayerInfo  FROM 'playerInfoData2017.csv' DELIMITER ',' CSV
-\copy DraftInfo  FROM 'draftResults2020.csv' DELIMITER ',' CSV
-\copy Career  FROM 'careerData2020.csv' DELIMITER ',' CSV
-\copy BodyMeasurements  FROM 'combineAnthro2020.csv' DELIMITER ',' CSV
-\copy StrengthAgility  FROM 'combineStrength2020.csv' DELIMITER ',' CSV
-\copy Shooting  FROM 'shootingStats2020.csv' DELIMITER ',' CSV
-\copy AverageGameStats  FROM 'gameStats2020.csv' DELIMITER ',' CSV
