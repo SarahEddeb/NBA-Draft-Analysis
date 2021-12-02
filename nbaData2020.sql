f8dee08ca43a4cd19bcbd39e7c0ce2a4db1b957e
Sarah Eddeb <saraheddeb@utmuser171-181.wireless.utoronto.ca>
2021-12-02 14:58:56 -0500

    Add nba data 2020 file

---
+++ nbaData2020.sql
@@ -0,0 +1,7 @@
+\copy PlayerInfo  FROM 'playerInfoData2020.csv' DELIMITER ',' CSV
+\copy DraftInfo  FROM 'draftResults2020.csv' DELIMITER ',' CSV
+\copy Career  FROM 'careerData2020.csv' DELIMITER ',' CSV
+\copy BodyMeasurements  FROM 'combineAnthro2020.csv' DELIMITER ',' CSV
+\copy StrengthAgility  FROM 'combineStrength2020.csv' DELIMITER ',' CSV
+\copy Shooting  FROM 'shootingStats2020.csv' DELIMITER ',' CSV
+\copy AverageGameStats  FROM 'gameStats2020.csv' DELIMITER ',' CSV
